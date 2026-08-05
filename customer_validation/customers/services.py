27388444211d1b00e742e1b28ae33ca4166c5d2a
ahsnafzal <cha84911@gmail.com>
2026-08-05 22:29:04 +0500

    Added services.py, validators.py and config.py and updated them with 3 knack fields mapping in config.py

---
+++ customer_validation/customers/services.py
@@ -0,0 +1,126 @@
+import requests
+
+from customer_validation.config import (
+    APP_ID,
+    API_KEY,
+    CUSTOMER_OBJECT,
+    RECORDS_OBJECT,
+    ISSUES_OBJECT,
+    CUSTOMER_FIELDS,
+    RECORD_FIELDS,
+    ISSUE_FIELDS
+)
+
+headers = {
+    "X-Knack-Application-Id": APP_ID,
+    "X-Knack-REST-API-Key": API_KEY,
+}
+
+CUSTOMER_URL = (
+    f"https://api.knack.com/v1/objects/{CUSTOMER_OBJECT}/records"
+    
+)
+RECORDS_URL = (
+    f"https://api.knack.com/v1/objects/{RECORDS_OBJECT}/records"
+)
+
+ISSUES_URL = (
+    f"https://api.knack.com/v1/objects/{ISSUES_OBJECT}/records"
+)
+                      #FETCHING DATA FROM KNACK
+def get_customers():
+
+    response = requests.get(
+        CUSTOMER_URL,
+        headers=headers
+    )
+
+    return response.json()
+
+
+#-----------------------------------------------------------
+############      SENDING DATA TO RECORDS TABLE    ###################
+def send_to_records(customer):
+
+    #EXTRACTING JUST EMAIL SEPARATELY FROM DICTIONARY, EMAIL IS A KEYWORD HERE
+    raw_email = customer.get(CUSTOMER_FIELDS["email"], "")
+    if isinstance(raw_email, dict):
+        email = raw_email.get("email", "")
+    else:
+        email = raw_email
+
+
+    #MAPPING DATA TO RECORDS TABLE FIELDS FROM CUSTOMERS TABLE
+    data = {
+        RECORD_FIELDS["customer_id"]: customer.get(CUSTOMER_FIELDS["customer_id"]),
+        RECORD_FIELDS["first_name"]: customer.get(CUSTOMER_FIELDS["first_name"]),   # First Name
+        RECORD_FIELDS["last_name"]: customer.get(CUSTOMER_FIELDS["last_name"]),   # Last Name
+        RECORD_FIELDS["email"]: email,                     # Email
+        RECORD_FIELDS["phone"]: customer.get(CUSTOMER_FIELDS["phone"]),   # Phone
+        RECORD_FIELDS["age"]: customer.get(CUSTOMER_FIELDS["age"]),   # Age
+        RECORD_FIELDS["country"]: customer.get(CUSTOMER_FIELDS["country"]),   # Country
+        RECORD_FIELDS["join_date"]: customer.get(CUSTOMER_FIELDS["join_date"]),   # Join Date
+        RECORD_FIELDS["balance"]: customer.get(CUSTOMER_FIELDS["balance"]),   # Balance
+        RECORD_FIELDS["status"]: customer.get(CUSTOMER_FIELDS["status"]),   # Status
+    }
+
+    response = requests.post(
+            RECORDS_URL,
+            headers=headers,
+            json=data
+        )
+    #print("Status Code:", response.status_code)
+    #print("Response Text:", response.text)
+
+    return response
+    
+
+
+#-----------------------------------------------------------
+############      SENDING DATA TO ISSUES TABLE    ###################
+def send_to_issues(customer, issues):
+#----------------------------------------------------
+###########     EXTRACTING JUST EMAIL SEPARATELY FROM DICTIONARY, EMAIL IS A KEYWORD HERE ############
+#-----------------------------------------------------
+    raw_email = customer.get(CUSTOMER_FIELDS["email"], "")
+    if isinstance(raw_email, dict):
+        email = raw_email.get("email", "")
+    else:
+        email = raw_email
+
+    ##  MERGING ISSUES TO STORE IN ONE FIELD JOINING THEM 
+    issue_detail = ", ".join(issues)
+
+    #MAPPING DATA TO ISSUES TABLE FIELDS 
+    data = {
+        ISSUE_FIELDS["customer_id"] : customer.get(CUSTOMER_FIELDS["customer_id"]),   #customer id
+        ISSUE_FIELDS["first_name"] : customer.get(CUSTOMER_FIELDS["first_name"]),   # First Name
+        ISSUE_FIELDS["last_name"] : customer.get(CUSTOMER_FIELDS["last_name"]),   # Last Name
+        ISSUE_FIELDS["email"] : email,                       # Email
+        ISSUE_FIELDS["phone"] : customer.get(CUSTOMER_FIELDS["phone"]),  # Phone
+        ISSUE_FIELDS["age"] : customer.get(CUSTOMER_FIELDS["age"]),   # Age
+        ISSUE_FIELDS["country"] : customer.get(CUSTOMER_FIELDS["country"]),   # Country
+        ISSUE_FIELDS["join_date"] : customer.get(CUSTOMER_FIELDS["join_date"]),   # Join Date
+        ISSUE_FIELDS["balance"] : customer.get(CUSTOMER_FIELDS["balance"]),   # Balance
+        ISSUE_FIELDS["status"] : customer.get(CUSTOMER_FIELDS["status"]),   # Status
+        ISSUE_FIELDS["issue_detail"] : issue_detail,
+
+    }
+#-----------------------------------------------
+#####    SENDING DATA TO KNACK ###########
+    response = requests.post(
+            ISSUES_URL,
+            headers=headers,
+            json=data
+        )
+
+    
+     # -----------------------------
+    # Print response for debugging
+    # -----------------------------
+    #print("Status Code:", response.status_code)
+    #print("Response Text:", response.text)
+
+    return response
+
+
--- customer_validation/customers/views.py
+++ customer_validation/customers/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .services import get_customers, send_to_records, send_to_issues
+from .validators import validate_customer
 
-# Create your views here.
+
+def customer_list(request):
+    """
+    Fetch customer records from Knack,
+    validate them and return the data.
+    """
+
+    # Get all customers from Knack
+    customers = get_customers()
+    
+
+    # Loop through every customer record
+    for customer in customers["records"]:
+
+        # Validate current customer
+        issues = validate_customer(customer)
+
+        if not issues:
+            send_to_records(customer)
+        else:
+            send_to_issues(customer, issues)
+
+        # Print validation result in terminal
+        #print(customer.get("field_139"), issues)
+
+    # Return original JSON response
+    return JsonResponse(customers)
