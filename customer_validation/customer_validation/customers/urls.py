15d29e33a5db136c4fc988313cbbdf28b6f6fc38
ahsnafzal <cha84911@gmail.com>
2026-08-18 18:11:58 +0500

    Reorganized project structure: removed duplicates, set up directories

--- customer_validation/customer_validation/customers/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from customers.views import customer_list
-from django.urls import path, include
-
-
-urlpatterns=[
-    path("", customer_list, name="customer_list"),
-]
--- customer_validation/customers/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from customers.views import  download, upload_validate
-from django.urls import path, include
-
-urlpatterns = [
-    path("download/", download, name="download"),
-    path("upload_validate/", upload_validate, name="upload_validate"),
-    
-]
